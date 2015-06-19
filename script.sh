a09efad0d5df325db175f515f75c1b86695aa30b
zpbappi <zpbappi@gmail.com>
2015-06-19 18:04:23 +1000

    removed _site/script

--- script.sh
+++ script.sh
@@ -6,12 +6,12 @@ git config user.name $GIT_NAME
 git config user.email $GIT_EMAIL
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-git add _site/*
+git add --all _site/*
 git commit -m "[ci skip] Travis Build - ${TRAVIS_BUILD_NUMBER}"
 if [ "$?" = "0" ]; then
 	git push origin HEAD:master
 else
-	echo "Nothing to commit" 1>&2
+	echo "Nothing to commit"
 fi
 rm -r .git/credentials
 exit 0
