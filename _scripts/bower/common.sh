9273eb183338e8a001ebb202ad78c0b5b8b4e441
zpbappi <zpbappi@gmail.com>
2015-06-26 22:47:38 +1000

    [ci skip] updated log message

--- _scripts/bower/common.sh
+++ _scripts/bower/common.sh
@@ -5,7 +5,7 @@
 
 # bower_publish version src_repo_slug
 bower_publish(){
-  echo "[INFO] Publishing version $1 to bower repo $2"
+  echo "[INFO] Publishing version $1 to bower repo $2-bower"
   version=$1
   src_repo_slug=$2
   gh_bower_repo_url=$(github_url_from_slug "$src_repo_slug-bower")
